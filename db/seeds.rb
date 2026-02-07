c501ee470853f18a15b9f8ba01b601692af1cad8
Anish Tewary <anishtewary@Anishs-MacBook-Air.local>
2026-02-07 18:00:45 +0530

    seed data temporary commented

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
-Room.create!([
-  { room_number: "301", room_type: "Classic_Harmony", base_price_per_night: 4500, status: "available" },
-  { room_number: "302", room_type: "Regal_Harmony", base_price_per_night: 3500, status: "available" },
-  { room_number: "303", room_type: "Regal_Harmony", base_price_per_night: 4000, status: "available" },
-  { room_number: "304", room_type: "Classic_Residence", base_price_per_night: 2500, status: "available" },
-  { room_number: "401", room_type: "Classic_Harmony", base_price_per_night: 4500, status: "available" },
-  { room_number: "402", room_type: "Regal_Harmony", base_price_per_night: 3500, status: "available" },
-  { room_number: "403", room_type: "Regal_Harmony", base_price_per_night: 4000, status: "available" },
-  { room_number: "404", room_type: "Classic_Residence", base_price_per_night: 2500, status: "available" },
-  { room_number: "200", room_type: "Banquet", base_price_per_night: 40000, status: "available" }
-])
+# Room.create!([
+#   { room_number: "301", room_type: "Classic_Harmony", base_price_per_night: 4500, status: "available" },
+#   { room_number: "302", room_type: "Regal_Harmony", base_price_per_night: 3500, status: "available" },
+#   { room_number: "303", room_type: "Regal_Harmony", base_price_per_night: 4000, status: "available" },
+#   { room_number: "304", room_type: "Classic_Residence", base_price_per_night: 2500, status: "available" },
+#   { room_number: "401", room_type: "Classic_Harmony", base_price_per_night: 4500, status: "available" },
+#   { room_number: "402", room_type: "Regal_Harmony", base_price_per_night: 3500, status: "available" },
+#   { room_number: "403", room_type: "Regal_Harmony", base_price_per_night: 4000, status: "available" },
+#   { room_number: "404", room_type: "Classic_Residence", base_price_per_night: 2500, status: "available" },
+#   { room_number: "200", room_type: "Banquet", base_price_per_night: 40000, status: "available" }
+# ])
